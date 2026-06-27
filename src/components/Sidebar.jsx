bcf6d626c9a81f27c6d0bde682536580c392c6f5
vatsalya vikram <oscillationary@vatsalyas-MacBook-Pro.local>
2026-06-27 13:01:14 +0530

    2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,67 @@
+import logo from "../assets/logo.png";
+
+import {
+  House,
+  Search,
+  BookOpen,
+  List,
+  Users,
+  Settings,
+} from "lucide-react";
+
+import { NavLink } from "react-router-dom";
+
 function Sidebar() {
   return (
-    <aside>
-      Sidebar
+    <aside className="sidebar">
+      <div className="logo">
+        <div className="logo">
+    <img src={logo} alt="Vinylog Logo" />
+</div>
+      </div>
+
+      <nav className="sidebar-nav">
+        <NavLink to="/">
+    <House size={20} />
+    <span>Home</span>
+</NavLink>
+
+        <NavLink to="/explore">
+          <Search size={20} />
+          <span>Discover</span>
+        </NavLink>
+
+        <NavLink to="/lists">
+          <BookOpen size={20} />
+          <span>Diary</span>
+        </NavLink>
+
+        <NavLink to="/lists">
+          <List size={20} />
+          <span>Lists</span>
+        </NavLink>
+
+        <NavLink to="/activity">
+          <Users size={20} />
+          <span>Activity</span>
+        </NavLink>
+      </nav>
+
+      <div className="sidebar-bottom">
+        <NavLink to="/settings">
+          <Settings size={20} />
+          <span>Settings</span>
+        </NavLink>
+
+        <div className="profile">
+          <div className="avatar"></div>
+
+          <div>
+            <h4>Vatsalya</h4>
+            <p>View Profile</p>
+          </div>
+        </div>
+      </div>
     </aside>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import "./styles/variables.css";
+import "./styles/layout.css";
+import "./styles/components.css";
+import App from "./App.jsx";
+import "./styles/sidebar.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
